15a92e3012869015a196f57558a5170504135d70
mysablehats <mysablehats@gmail.com>
2018-02-20 09:54:47 +0000

    added additional in layer conditioning

---
+++ private/cond_MS_.m
@@ -0,0 +1,32 @@
+function [newskel, out] = cond_MS_(tdskel, skelldef)
+angle = [];
+inv_polar = skelldef.ic;
+[avp, newskel, ac,iac,av,eha] = inv_polar(tdskel, skelldef, angle);
+%%%%%%%% test version
+out.angle = avp;
+out.endheadangle = ac;
+out.initialheadangle = iac;
+out.initialhipchestangle = av;
+out.endhipchestangle = eha;
+
+%out = angle;
+if 0% (avp(1)>0.3&&isequal(inv_polar,@polar_core))||(avp(1)<0.8&&isequal(inv_polar,@inv_core))
+    oldpath = addpath('misc','misc\measures');
+    figure
+    skeldraw(tdskel(1:15,:),'f');hold on;
+    rangerange(1,:) = linspace(0,avp(1));
+    rangerange(2,:) = linspace(0,avp(2));
+    rangerange(3,:) = linspace(0,avp(3));
+    arrayofskels = zeros([size(tdskel),100]);
+    for i = 1:100
+        [~, newskel] = inv_polar(tdskel, skelldef, rangerange(:,i));
+        
+        arrayofskels(:,:,i) = newskel;
+    end
+    skeldraw(arrayofskels(1:15,:,:),'W');
+    path(oldpath)
+end
+
+end
+
+
--- private/cond_inp.m
+++ private/cond_inp.m
@@ -1,4 +1,4 @@
-function [ inp ] = cond_inp( inp, conds )
+function [ oups, oy ] = cond_inp( inps,iy, conds, skelldef )
 %cond_inp Conditions input data for each layer of the classifier
 %   This function is supposed to read arq_conn and go through all the
 %   conditioning arguments and apply them to the input after it is
@@ -9,25 +9,91 @@ confuns = [];
 for i = 1:length(conds)
     switch conds{i}
         case 'ms'
-            confuns{i} = @mirrorsagittal;            
+            confuns{i} = @mirrorsagittal; 
+            oy = [iy iy];
+            oups.input_ends = [inps.input_ends inps.input_ends];
+            if ~isempty(inps.oldwhotokill)
+                error('Oldwhotokill is being used and the sagittal mirroring was not prepared to deal with it. It is probably just a matter of repeating the indexes with an offset, but this was not implemented and the algorithm will fail.')
+            else 
+                oups.oldwhotokill = inps.oldwhotokill; 
+            end
+            % do these mirrored data require a new index or not? At the
+            % moment i don't think so, so i will just copy them over
+            if 1
+                oups.index = [inps.index inps.index];                
+            else % the alternative is
+                oups.index = [inps.index inps.index+size(inps.index,2)];
+            end
+            oups.awk = inps.awk;
         case 'rd'
             confuns{i} = @removedoubled;
         otherwise
             error('wrong argument in cond_inpu: unrecognized conditioning procedure')
     end
 end
+skelldef.ic = @inv_core;
 for j = confuns
     dbgmsg(['Applying ' j ' to input'])
-    for k = 1:size(inp,2) % check it
-        inp(:,k) = j{:}(inp(:,k)); 
-    end 
+    if isequal(inps.input,inps.input_clip)
+        inp = zeros(size(inps.input));
+        [~,outf] = j{:}(inps.input(:,1),skelldef);
+        out = repmat(outf,size(inps.input,2),1);
+        for k = 1:size(inp,2) % check it
+            [inp(:,k),out(k)] = j{:}(inps.input(:,k),skelldef);
+        end
+        inpC = inp;
+        for ii = 1:size(inps.input,2) 
+            ihca(ii,:) = out(ii).initialhipchestangle;
+            ehca(ii,:) = out(ii).endhipchestangle;
+            iha(ii,:) = out(ii).initialheadangle;
+            eha(ii,:) = out(ii).endheadangle;      
+            ehcar(ii,:) = out(ii).endhipchestanglerho;
+            ehar(ii,:) = out(ii).endheadanglerho;     
+        end
+        a = vec2ind(iy);
+        figure
+        scatter3(ihca(:,1), ihca(:,2), ihca(:,3),2,a )
+        hold on
+        scatter3(iha(:,1), iha(:,2), iha(:,3),2,a )
+        figure
+        scatter3(ehca(:,1), ehca(:,2), ehca(:,3),2,a )
+        hold on
+        scatter3(eha(:,1), eha(:,2), eha(:,3),2,a )
+        figure
+        scatter3(ehcar(:,1), ehcar(:,2), ehcar(:,3),2,a )
+        hold on
+        scatter3(ehar(:,1), ehar(:,2), ehar(:,3),2,a )
+        
+    else
+        warning('check if input and input_clip are both necessary. this function is performing actions on both and could run twice as fast if only one of that needs to be conditioned.')
+        inp = zeros(size(inps.input));
+        inpC = zeros(size(inps.input_clip));
+        [~,outf] = j{:}(inps.input(:,1),skelldef);
+        out = repmat(outf,size(inps.input,2),1);
+        outC = repmat(outf,size(inps.input_clip,2),1);
+        for k = 1:size(inp,2) % check it
+            [inp(:,k),out(k)] = j{:}(inps.input(:,k),skelldef);
+            [inpC(:,k),outC(k)] = j{:}(inps.input_clip(:,k),skelldef);
+        end
+    end
+    if isequal(j{:},@mirrorsagittal)
+        oups.input_clip = [inps.input_clip inpC];
+        oups.input = [inps.input inp];
+    end
 end
 end
-function inpk = mirrorsagittal(inpk)
-disp('hello1')
+function [inpk,out] = mirrorsagittal(inpk,skelldef)
+%%% old notation says this should be a tensor. since i am not getting the
+%%% layer type, i will not try to guess what this is. for velocities also
+%%% this will be completely messed up, since i should actually create a
+%%% angle of shift vector from positions and use that to flip velocities. 
+%%% Actually the whole angle vector might be interesting to create, since I
+%%% want to know if that carries a lot of information or not. 
+[inpks,out] =cond_MS_(reshape(inpk,[],3),skelldef);
+ inpk = reshape(inpks,[],1);
 end
-function inpk = removedoubled(inpk)
-disp('hello2')
+function inpk = removedoubled(inpk,skelldef)
+error('not implemented yet')
 end
 
 
